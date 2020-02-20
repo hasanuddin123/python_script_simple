e8bab3e061e127aa072421f8485d431f8abf1592
hasanuddin123 <50686710+hasanuddin123@users.noreply.github.com>
2020-02-20 10:58:02 +0700

    Create cek_data_sama.py

---
+++ cek_data_sama.py
@@ -0,0 +1,12 @@
+###framework django
+'''
+Mengambil data jika ada no ktp yang sama, sample model namanya Konsumen
+'''
+
+from django.db.models import Count
+from .models import Konsumen
+
+cek = Konsumen.objects.values('noktp').annotate(name_count=Count('noktp')).filter(name_count__gt=1)
+cek_2 = Konsumen.objects.filter( noktp__in=[item['noktp'] for item in cek ])
+cek_3 = ([ int(item.id) for item in cek_2 ])
+hasil = Konsumen.objects.filter(pk__in=cek_3)
